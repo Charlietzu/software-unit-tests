ee563f00122baef89000603ae490bfac608754ac
Charlietzu <caiocesarsilva08@gmail.com>
2021-09-14 20:21:32 -0300

    feat: implemented custom mocks

--- Features.Tests/04 - Human Data/EmployeeBogusTestsFixture.cs
+++ Features.Tests/04 - Human Data/EmployeeBogusTestsFixture.cs
@@ -2,6 +2,8 @@
 using Bogus.DataSets;
 using Features.Employees;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 using static Bogus.DataSets.Name;
 
@@ -13,6 +15,21 @@ namespace Features.Tests
     {
         public Employee CreateValidEmployee()
         {
+            return CreateEmployees(1, true).FirstOrDefault();
+        }
+
+        public IEnumerable<Employee> CreateVariedEmployees()
+        {
+            var employees = new List<Employee>();
+
+            employees.AddRange(CreateEmployees(50, true).ToList());
+            employees.AddRange(CreateEmployees(50, false).ToList());
+
+            return employees;
+        }
+
+        public IEnumerable<Employee> CreateEmployees(int quantity, bool active)
+        {
             Gender gender = new Faker().PickRandom<Name.Gender>();
 
             return new Faker<Employee>("pt_BR")
@@ -23,9 +40,9 @@ namespace Features.Tests
                 f.Date.Past(80, DateTime.Now.AddYears(-18)),
                 DateTime.Now,
                 "",
-                true
+                active
                     )).RuleFor(e => e.Email, (f, e) =>
-                    f.Internet.Email(e.FirstName.ToLower(), e.LastName.ToLower()));
+                    f.Internet.Email(e.FirstName.ToLower(), e.LastName.ToLower())).Generate(quantity);
         }
 
         public Employee CreateInvalidEmployee()
---
+++ Features.Tests/05 - Mock/EmployeeServiceTests.cs
@@ -0,0 +1,85 @@
+﻿using Features.Employees;
+using MediatR;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Xunit;
+
+namespace Features.Tests
+{
+    [Collection(nameof(EmployeeBogusCollection))]
+    public class EmployeeServiceTests
+    {
+        readonly EmployeeBogusTestsFixture _employeeBogusTestsFixture;
+
+        public EmployeeServiceTests(EmployeeBogusTestsFixture employeeBogusTestsFixture)
+        {
+            _employeeBogusTestsFixture = employeeBogusTestsFixture;
+        }
+
+
+        [Fact(DisplayName = "Add Employee with Success")]
+        [Trait("Category", "Employee Service Mock Tests")]
+        public void EmployeeService_Add_MustExecuteWithSuccess()
+        {
+            // Arrange
+            Employee employee = _employeeBogusTestsFixture.CreateValidEmployee();
+            Mock<IEmployeeRepository> employeeRepo = new Mock<IEmployeeRepository>();
+            Mock<IMediator> mediatr = new Mock<IMediator>();
+
+            EmployeeService employeeService = new EmployeeService(employeeRepo.Object, mediatr.Object);
+
+            // Act
+            employeeService.Add(employee);
+
+            // Assert
+            employeeRepo.Verify(r => r.Add(employee), Times.Once);
+            mediatr.Verify(m => m.Publish(It.IsAny<INotification>(), CancellationToken.None),
+                           Times.Once);
+        }
+
+        [Fact(DisplayName = "Add Employee with Failure")]
+        [Trait("Category", "Employee Service Mock Tests")]
+        public void EmployeeService_Add_MustFailDueToInvalidEmployee()
+        {
+            // Arrange
+            Employee employee = _employeeBogusTestsFixture.CreateInvalidEmployee();
+            Mock<IEmployeeRepository> employeeRepo = new Mock<IEmployeeRepository>();
+            Mock<IMediator> mediatr = new Mock<IMediator>();
+
+            EmployeeService employeeService = new EmployeeService(employeeRepo.Object, mediatr.Object);
+
+            // Act
+            employeeService.Add(employee);
+
+            // Assert
+            employeeRepo.Verify(r => r.Add(employee), Times.Never);
+            mediatr.Verify(m => m.Publish(It.IsAny<INotification>(), CancellationToken.None),
+                           Times.Never);
+        }
+
+        [Fact(DisplayName = "Get Active Employees")]
+        [Trait("Category", "Employee Service Mock Tests")]
+        public void EmployeeService_GetAllActive_MustReturnOnlyActiveEmployees()
+        {
+            // Arrange
+            Mock<IEmployeeRepository> employeeRepo = new Mock<IEmployeeRepository>();
+            Mock<IMediator> mediatr = new Mock<IMediator>();
+
+            employeeRepo
+                .Setup(e => e.GetAll())
+                .Returns(_employeeBogusTestsFixture.CreateVariedEmployees());
+
+            EmployeeService employeeService = new EmployeeService(employeeRepo.Object, mediatr.Object);
+
+            // Act
+            IEnumerable<Employee> employees = employeeService.GetAllActive();
+
+            // Assert
+            employeeRepo.Verify(r => r.GetAll(), Times.Once);
+            Assert.True(employees.Any());
+            Assert.False(employees.Count(e => !e.Active) > 0);
+        }
+    }
+}
